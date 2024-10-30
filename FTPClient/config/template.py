625bdc6e91adb8e48a9f40e9b4c3b4593fc04d6e
Terry Zhang <77920425+RookieTerry@users.noreply.github.com>
2024-10-30 01:21:32 +0000

    Create template.py

---
+++ FTPClient/config/template.py
@@ -0,0 +1,21 @@
+START_MENU = """
+---------------------------------------------
+              欢迎使用FTP服务器系统
+---------------------------------------------
+"""
+
+LOGINED_MENU = """
+------------------------------------------------------------------------------------------------
+                                    FTP   CLIENT
+
+User:{0}         TotalSpace:{1} MB         UserSpace:{2} MB
+------------------------------------------------------------------------------------------------
+Commands:
+    put:    put [filename]     # upload a file to server,[filename] must have a full path name
+    get:    get [filename]     # download a file from server
+    ls:     ls                 # show the folder and files in the home folder
+    cd:     cd [folder]        # go to [folder],return back input cd ..
+    mkdir:  mkdir [folder]     # Create a directory
+    delete: delete [folder]    # Delete folders or files
+    quit:                      # exit system
+"""
